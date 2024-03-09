895c412fee839797f0824f65864238eef2d60e73
xcheche <chekwubechukwu23@gmail.com>
2024-03-09 16:33:51 +0100

    in progress

--- lib/authentication/login_screen.dart
+++ lib/authentication/login_screen.dart
@@ -1,17 +1,90 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:users_app/widgets/loading_dialog.dart';
 
+import '../global/global_var.dart';
+import '../methods/common_methods.dart';
+import '../pages/home_page.dart';
 import 'signup_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
-  State<LoginScreen> createState() => _SignUpScreenState();
+  State<LoginScreen> createState() => _LoginScreenState();
 }
 
-class _SignUpScreenState extends State<LoginScreen> {
-  TextEditingController emailtextEditingController = TextEditingController();
-  TextEditingController passwordtextEditingController = TextEditingController();
+class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController emailTextEditingController = TextEditingController();
+  TextEditingController passwordTextEditingController = TextEditingController();
+  CommonMethods cMethods = CommonMethods();
+
+  checkIfNetworkIsAvailable() {
+    cMethods.checkConnectivity(context);
+
+    signInFormValidation();
+  }
+
+  signInFormValidation() {
+    if (!emailTextEditingController.text.contains("@")) {
+      cMethods.displaySnackBar("please write valid email.", context);
+    } else if (passwordTextEditingController.text.trim().length < 5) {
+      cMethods.displaySnackBar(
+          "your password must be atleast 6 or more characters.", context);
+    } else {
+      signInUser();
+    }
+  }
+
+  signInUser() async {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) =>
+          LoadingDialog(messageText: "Allowing you to Login..."),
+    );
+
+    final User? userFirebase = (await FirebaseAuth.instance
+            .signInWithEmailAndPassword(
+      email: emailTextEditingController.text.trim(),
+      password: passwordTextEditingController.text.trim(),
+    )
+            .catchError((errorMsg) {
+      Navigator.pop(context);
+      cMethods.displaySnackBar(errorMsg.toString(), context);
+    }))
+        .user;
+
+    if (!context.mounted) return;
+    // ignore: use_build_context_synchronously
+    Navigator.pop(context);
+
+    if (userFirebase != null) {
+      DatabaseReference usersRef = FirebaseDatabase.instance
+          .ref()
+          .child("users")
+          .child(userFirebase.uid);
+      usersRef.once().then((snap) {
+        if (snap.snapshot.value != null) {
+          if ((snap.snapshot.value as Map)["blockStatus"] == "no") {
+            userName = (snap.snapshot.value as Map)["name"];
+            Navigator.push(
+                context, MaterialPageRoute(builder: (c) => const HomePage()));
+          } else {
+            FirebaseAuth.instance.signOut();
+            cMethods.displaySnackBar(
+                "you are blocked. Contact admin: checheomenife@gmail.com",
+                context);
+          }
+        } else {
+          FirebaseAuth.instance.signOut();
+          cMethods.displaySnackBar(
+              "your record do not exists as a User.", context);
+        }
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,63 +94,82 @@ class _SignUpScreenState extends State<LoginScreen> {
           padding: const EdgeInsets.all(10),
           child: Column(
             children: [
-              Image.asset('assets/images/logo.png'),
+              Image.asset("assets/images/logo.png"),
+
               const Text(
-                'Login as a User',
-                style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+                "Login as a User",
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-              //textfield-buton
+
+              //text fields + button
               Padding(
                 padding: const EdgeInsets.all(22),
                 child: Column(
                   children: [
                     TextField(
-                      controller: emailtextEditingController,
+                      controller: emailTextEditingController,
                       keyboardType: TextInputType.emailAddress,
                       decoration: const InputDecoration(
-                        labelText: 'Email',
-                        labelStyle: TextStyle(fontSize: 14, color: Colors.grey),
+                        labelText: "User Email",
+                        labelStyle: TextStyle(
+                          fontSize: 14,
+                        ),
+                      ),
+                      style: const TextStyle(
+                        color: Colors.grey,
+                        fontSize: 15,
                       ),
                     ),
-                    const SizedBox(height: 22),
+                    const SizedBox(
+                      height: 22,
+                    ),
                     TextField(
-                      controller: passwordtextEditingController,
+                      controller: passwordTextEditingController,
                       obscureText: true,
-                      keyboardType: TextInputType.emailAddress,
+                      keyboardType: TextInputType.text,
                       decoration: const InputDecoration(
-                        labelText: 'Password',
-                        labelStyle: TextStyle(fontSize: 14, color: Colors.grey),
+                        labelText: "User Password",
+                        labelStyle: TextStyle(
+                          fontSize: 14,
+                        ),
+                      ),
+                      style: const TextStyle(
+                        color: Colors.grey,
+                        fontSize: 15,
                       ),
                     ),
-                    const SizedBox(height: 32),
+                    const SizedBox(
+                      height: 32,
+                    ),
                     ElevatedButton(
+                      onPressed: () {
+                        checkIfNetworkIsAvailable();
+                      },
                       style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.purple,
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 80, vertical: 20),
-                      ),
-                      onPressed: () {},
-                      child: const Text(
-                        'Login',
-                      ),
+                          backgroundColor: Colors.purple,
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 80, vertical: 10)),
+                      child: const Text("Login"),
                     ),
                   ],
                 ),
               ),
-              //already have an account
 
               const SizedBox(
                 height: 12,
               ),
+
+              //textbutton
               TextButton(
                 onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const SignUpScreen()));
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (c) => const SignUpScreen()));
                 },
                 child: const Text(
-                  'Don\'t have an account? Register here',
+                  "Don\'t have an Account? Register Here",
                   style: TextStyle(
                     color: Colors.grey,
                   ),
